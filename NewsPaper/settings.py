fe7c54db1d5089c10f5328d4535b474e1098ad62
Aleksey <arekabu@yandex.com>
2025-03-26 17:14:52 +0300

    Module_21_Unit_02_complete.

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -46,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.flatpages',
     'fpages',
-    'posts',
+    'posts.apps.NewsConfig',
     'django_filters',
     'protect',
     'sign',
@@ -179,3 +182,11 @@ ACCOUNT_FORMS = {'signup': 'posts.forms.BasicSignupForm'}
 #         }
 #     }
 # }
+
+EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_PORT = '465'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True
+SITE_URL = 'http://127.0.0.1:8000'
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
---
+++ posts/signals.py
@@ -0,0 +1,43 @@
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+from .models import PostCategory
+from NewsPaper import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+def send_notifications(preview, pk, title, subscribers, username):
+    html_content = render_to_string(
+        'post_created_email.html',
+        {
+            'username': username,
+            'title': title,
+            'text': preview,
+            'link': f'{settings.SITE_URL}/news/{pk}'
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject=title,
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers,
+    )
+
+    msg.attach_alternative(html_content, 'text/html')
+
+    msg.send()
+
+@receiver(m2m_changed, sender=PostCategory)
+def notify_about_new_post(sender, instance, **kwargs):
+    if kwargs['action'] == 'post_add':
+        categories = instance.category.all()
+        # subscribers_emails = []
+
+        for cat in categories:
+            subscribers = cat.subscribers.all()
+            # subscribers_emails += [s.email for s in subscribers]
+            for sub in subscribers:
+                send_notifications(instance.preview(), instance.pk, instance.title, [sub.email], sub.username)
+                print(list(sub.email), sub.username)
+
+        # send_notifications(instance.preview(), instance.pk, instance.title, subscribers_emails)
