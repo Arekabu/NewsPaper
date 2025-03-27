52e920dde92462980bfd007b3590841da1ea6794
Aleksey <arekabu@yandex.com>
2025-03-27 13:20:49 +0300

    Module_21_Unit_4_done

--- posts/signals.py
+++ posts/signals.py
@@ -1,9 +1,11 @@
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, pre_save
 from django.dispatch import receiver
-from .models import PostCategory
-from NewsPaper import settings
+from .models import PostCategory, Post
+from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from datetime import datetime, timezone
+from django.core.exceptions import ValidationError
 
 def send_notifications(preview, pk, title, subscribers, username):
     html_content = render_to_string(
@@ -40,3 +42,9 @@ def notify_about_new_post(sender, instance, **kwargs):
                 send_notifications(instance.preview(), instance.pk, instance.title, [sub.email], sub.username)
 
         # send_notifications(instance.preview(), instance.pk, instance.title, subscribers_emails)
+
+@receiver(pre_save, sender=Post)
+def day_post_counter(sender, instance, **kwargs):
+    posts_today = Post.objects.filter(author=instance.author, date__gte=datetime.now(timezone.utc).date()).count()
+    if posts_today >=3:
+        raise ValidationError("Вы можете публиковать максимум 3 поста в сутки!")
