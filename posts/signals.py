7f3fb1b908534d65120daf25602adfd0d3a0cfc9
Aleksey <arekabu@yandex.com>
2025-03-27 14:02:46 +0300

    404_custom_error_added

--- posts/signals.py
+++ posts/signals.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from datetime import datetime, timezone
-from django.core.exceptions import ValidationError
+from django.core.exceptions import PermissionDenied
 
 def send_notifications(preview, pk, title, subscribers, username):
     html_content = render_to_string(
@@ -47,4 +47,4 @@ def notify_about_new_post(sender, instance, **kwargs):
 def day_post_counter(sender, instance, **kwargs):
     posts_today = Post.objects.filter(author=instance.author, date__gte=datetime.now(timezone.utc).date()).count()
     if posts_today >=3:
-        raise ValidationError("Вы можете публиковать максимум 3 поста в сутки!")
+        raise PermissionDenied( "Вы можете публиковать максимум 3 поста в сутки!")
