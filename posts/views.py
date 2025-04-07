7321d247cbddd3fe08333d1fc75cfbf5fcc20a43
Aleksey <arekabu@yandex.com>
2025-04-07 16:42:04 +0300

    Module_23_Final_Task

--- posts/views.py
+++ posts/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.decorators import login_required
 from .tasks import new_post_notification
+from django.core.cache import cache
 
 class PostsList(ListView):
     # Указываем модель, объекты которой мы будем выводить
@@ -30,6 +31,14 @@ class PostDetail(DetailView):
     template_name = 'post.html'
     # Название объекта, в котором будет выбранный пользователем продукт
     context_object_name = 'post'
+
+    def get_object(self, *args, **kwargs ):
+        obj = cache.get(f'post-{self.kwargs["pk"]}', None)
+        if not obj:
+            obj = super().get_object(queryset=self.queryset)
+            cache.set(f'post-{self.kwargs["pk"]}', obj)
+
+        return obj
 
 class PostSearch(ListView):
     model = Post
