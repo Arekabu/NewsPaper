ac32460b682acad5aefb96595205657526f60e6f
Aleksey <arekabu@yandex.com>
2025-05-20 13:57:48 +0300

    Module_29 Unit_04 END

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -44,6 +44,7 @@ AUTHENTICATION_BACKENDS = [
 
 # Application definition
 INSTALLED_APPS = [
+    'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
