1406caf2ed093b6242425a9e2fb5b39f8bac76c0
Aleksey <arekabu@yandex.com>
2025-05-26 14:01:30 +0300

    Module_30_Unit_04_end

--- NewsPaper/urls.py
+++ NewsPaper/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from posts.views import Index, PostsList
 
 urlpatterns = [
@@ -28,4 +29,8 @@ urlpatterns = [
     path('', include('protect.urls')),
     path('index/', Index.as_view(), name='post_index'),
     path('i18n/', include('django.conf.urls.i18n')),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+        ), name='swagger-ui'),
 ]
